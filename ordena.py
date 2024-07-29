4998c011805894d84af4b5ec156b0a3b3ef1f39a
Williano Serpa <willyudi@gmail.com>
2024-07-29 13:49:04 +0000

    commit

--- ordena.py
+++ ordena.py
@@ -1,3 +1,4 @@
 # Escreva uma função em Python que ordena em ordem alfabética palavras em uma string.
 # Entrada: Uma string com palavras separadas por espaço
 # Saída: Uma string com as palavras em ordem alfabética
+# 
